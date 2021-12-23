269875bf129003778533183910434ebfc7f220df
Gabriel <gabriel.alves.marques@hotmail.com>
2021-12-22 23:47:57 -0300

    Formulários e Navegação

--- App.tsx
+++ App.tsx
@@ -10,7 +10,9 @@ import {
 } from '@expo-google-fonts/poppins';
 
 import theme from './src/global/styles/theme';
-import { Dashboard } from './src/screens/Dashboard';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { AppRoutes } from './src/routes/app.routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -25,7 +27,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -12,6 +12,7 @@ import {
     User,
     UserGreeting,
     UserName,
+    LogoutButton,
     Icon,
     HighlightCards,
     Transactions,
@@ -71,7 +72,9 @@ export function Dashboard() {
                             <UserName>Gabriel</UserName>
                         </User>
                     </UserInfo>
-                    <Icon name="power" />
+                    <LogoutButton onPress={() => { }}>
+                        <Icon name="power" />
+                    </LogoutButton>
                 </UserWrapper>
             </Header>
 
